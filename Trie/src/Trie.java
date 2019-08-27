69cb133f1e354bb99eaa8dffb2ad4ec8ca4db8d9
xgblack <gmg@xgblack.cn>
2019-08-27 22:34:53 +0800

    前缀搜索

--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -71,4 +71,21 @@ public class Trie {
         return cur.isWord;
     }
 
+    /**
+     * 在Trie中是否有单词以prefix为前缀
+     * @param prefix
+     * @return
+     */
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+
 }
